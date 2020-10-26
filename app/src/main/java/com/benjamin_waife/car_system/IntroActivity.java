fc47e744a2a585607568526a68d9a97b07576921
Benjamin Waife <bwaife@gmail.com>
2020-10-26 03:33:00 +0000

    V2 Splash Screen and Walkthrough done

---
+++ app/src/main/java/com/benjamin_waife/car_system/IntroActivity.java
@@ -0,0 +1,72 @@
+package com.benjamin_waife.car_system;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.text.Html;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.benjamin_waife.car_system.HelperClass.SlideAdapter;
+
+public class IntroActivity extends AppCompatActivity {
+
+    ViewPager viewPager;
+    LinearLayout dotsLayout;
+
+    SlideAdapter slideAdapter;
+    TextView[] dots;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        viewPager = findViewById(R.id.slider);
+        dotsLayout = findViewById(R.id.dots);
+
+        //Call Adapter
+        slideAdapter = new SlideAdapter(this);
+        viewPager.setAdapter(slideAdapter);
+
+        addDots(0);
+        viewPager.addOnPageChangeListener(changeListener);
+
+    }
+
+    private void addDots(int position) {
+        dots = new TextView[4];
+        dotsLayout.removeAllViews();
+
+        for (int i = 0; i < dots.length; i++) {
+            dots[i] = new TextView(this);
+            dots[i].setText(Html.fromHtml("&#8226"));
+            dots[i].setTextSize(35);
+
+            dotsLayout.addView(dots[i]);
+
+        }
+        if (dots.length > 0){
+            dots[position].setTextColor(getResources().getColor(R.color.colorPrimaryDark));
+        }
+    }
+
+    ViewPager.OnPageChangeListener changeListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            addDots(position);
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+
+    };
+}
--- app/src/main/java/com/benjamin_waife/car_system/MainActivity.java
+++ app/src/main/java/com/benjamin_waife/car_system/MainActivity.java
@@ -2,13 +2,51 @@ package com.benjamin_waife.car_system;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN = 2500;
+
+    //Variables
+    Animation topAnim, bottomAnim;
+    TextView name, slogan;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+     /*   //Animations
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);*/
+
+        //logo and slogan
+        name = findViewById(R.id.name);
+        slogan = findViewById(R.id.slogan);
+
+        name.setAnimation(topAnim);
+        slogan.setAnimation(bottomAnim);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, IntroActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_SCREEN);
+
     }
 }
